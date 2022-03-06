0dd2decb1a89d4d2f7184ef43011b9fd65e1cfd7
K1W1-SRB <93095920+K1W1-SRB@users.noreply.github.com>
2022-03-06 11:44:52 +0000

    Add files via upload

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function Hero() {
+    return (
+        <div className="hero">
+            < img className="hero--image" src={process.env.PUBLIC_URL + "/images/photo-grid.png"} />
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
